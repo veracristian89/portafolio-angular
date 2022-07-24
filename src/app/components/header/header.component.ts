be1945d2cd68951c2c9849333d621b6248944eec
Cristian <veracristian89@gmail.com>
2022-07-24 19:42:15 -0300

    firebase

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  titulo : string = "Cristian Vera"
   constructor() { }
 
   ngOnInit(): void {
