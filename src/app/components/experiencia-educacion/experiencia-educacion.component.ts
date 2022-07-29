4ca24649c48f99b8745e9fdc98432221a16e00eb
Cristian <veracristian89@gmail.com>
2022-07-29 18:22:40 -0300

    se agrega simbolo mas con fontawesome en experiencia y educacion, se agrega item en educacion

--- src/app/components/experiencia-educacion/experiencia-educacion.component.ts
+++ src/app/components/experiencia-educacion/experiencia-educacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experiencia-educacion',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia-educacion.component.scss']
 })
 export class ExperienciaEducacionComponent implements OnInit {
-
+  faPlus = faPlus;
   constructor() { }
 
   ngOnInit(): void {
